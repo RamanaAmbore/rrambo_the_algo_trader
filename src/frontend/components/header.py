7fca00146fcfd1db304dab075d0c03394a043944
Ramana Ambore <ramana.ambore@gmail.com>
2025-05-03 22:46:48 +0530

    Hamburger menu related changes in progress - 1

--- src/frontend/components/header.py
+++ src/frontend/components/header.py
@@ -1,4 +1,4 @@
-from dash import html, dcc, Input, Output
+from dash import html, dcc, Input, Output, callback_context
 import dash
 from src.frontend.settings.config import NAV_LINKS
 
@@ -6,31 +6,28 @@ class HeaderComponent:
     @staticmethod
     def create():
         return html.Div([
-            dcc.Location(id='url', refresh=False),
+        dcc.Location(id='url', refresh=False),
 
-            html.Div([
-                html.Div("\u2630", className="hamburger-icon", id="hamburger-icon"),  # Hamburger icon
-                html.Div(
-                    className='navbar',  # Sidebar navbar
-                    id='navbar',
-                    children=[
-                        html.Img(src="assets/logo1.png", alt="Rambo Logo"),
-                        html.Div(
-                            className='nav-links',
-                            id='nav-links',
-                            children=[
-                                dcc.Link(
-                                    html.Div(text, id={'type': 'nav-link', 'index': href}),
-                                    href=href,
-                                    className="nav-link"
-                                )
-                                for text, href in NAV_LINKS
-                            ]
-                        )
-                    ]
-                )
-            ], className="sidebar-wrapper")
-        ])
+        html.Div([
+            html.Div([  # Top bar: logo left, hamburger right
+                html.Div(html.Img(src="assets/logo1.png", alt="Rambo Logo"), className="logo-container"),
+                html.Div("\u2630", className="hamburger-icon", id="hamburger-icon")
+            ], className="top-row"),
+
+            html.Div(  # Nav links section
+                id="nav-links",
+                className="nav-links",
+                children=[
+                    dcc.Link(
+                        html.Div(text, id={'type': 'nav-link', 'index': href}),
+                        href=href,
+                        className="nav-link"
+                    )
+                    for text, href in NAV_LINKS
+                ]
+            )
+        ], className="navbar-row")
+    ])
 
     @staticmethod
     def register_callbacks(app):
@@ -39,13 +36,21 @@ class HeaderComponent:
             Input('url', 'pathname')
         )
         def update_active_links(pathname):
-            return ['nav-link active' if pathname == href else 'nav-link' 
+            return ['nav-link active' if pathname == href else 'nav-link'
                    for text, href in NAV_LINKS]
 
         @app.callback(
-            Output('navbar', 'className'),
+            Output('nav-links', 'className'),
             Input('hamburger-icon', 'n_clicks'),
             prevent_initial_call=True
         )
-        def toggle_navbar(n):
-            return 'navbar show' if n % 2 != 0 else 'navbar'
+        def toggle_nav_links(n):
+            triggered = callback_context.triggered
+            if not triggered:
+                return 'nav-links'
+
+            current_classes = callback_context.states.get('nav-links.className', '')
+            if 'show' in current_classes:
+                return 'nav-links'  # Hide
+            else:
+                return 'nav-links show'  # Show
